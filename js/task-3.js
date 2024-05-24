7ca81a2625380a311951dafa40db9a945b61653a
nihkumkram <koteya754@gmail.com>
2024-05-24 19:26:22 +0200

    gj

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+    return true;
+  } else {
+    return false;
+  }
+}
